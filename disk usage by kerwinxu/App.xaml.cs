26863b2e9e4093bdc032367f9f29b7a870d17720
恒晓 徐 <kerwin.cn@gmail.com>
2022-08-15 19:35:29 +0800

    添加项目文件。

---
+++ disk usage by kerwinxu/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace disk_usage_by_kerwinxu
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ disk usage by kerwinxu/Model/FolderItem.cs
@@ -0,0 +1,71 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace disk_usage_by_kerwinxu.Model
+{
+    public  class FolderItem: ObservableObject
+    {
+
+
+        private FileSystemInfo _info;
+        /// <summary>
+        /// 目录和文件夹都用这个。
+        /// </summary>
+        public FileSystemInfo Info { 
+            get { return _info; }
+            set { _info = value; RaisePropertyChanged(() => Info); }
+        }
+
+
+        private bool _is_folder;
+        /// <summary>
+        /// 是否是文件夹
+        /// </summary>
+        public bool IsFolder
+        {
+            get { return _is_folder; }
+            set { _is_folder = value; RaisePropertyChanged(() => IsFolder); }
+        }
+
+        private List<FolderItem> _children = new List<FolderItem>();
+        /// <summary>
+        /// 这个代表子项目
+        /// </summary>
+        public List<FolderItem> Children
+        {
+            get { return _children; }
+
+        }
+
+        private long _size;
+        /// <summary>
+        /// 返回尺寸
+        /// </summary>
+        public long Size
+        {
+            get
+            {
+                if (IsFolder)
+                {
+                    return Children.Select(x => x.Size).Sum();
+
+                }
+                else
+                {
+                    return _size;
+                }
+            }
+            set
+            {
+                _size = value; RaisePropertyChanged(() => Size);
+            }
+        }
+        
+
+    }
+}
